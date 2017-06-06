f91142b23192e900864fa0fef58b5bd453a80d9b
Unknown <leafar_rafaelsm@hotmail.com>
2017-06-06 20:03:21 -0300

    Criado js para cadastrar cliente(adm)

---
+++ javascript/adm.js
@@ -0,0 +1,6 @@
+function cadastrarCliente(){
+	name = document.getElementById('name').value;
+	email = document.getElementById('email').value;
+	password = document.getElementById('password').value;
+	cpassword = document.getElementById('cpassword').value;
+}
