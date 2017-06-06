0e273b9c8afc2a5c07fe36d209d7574add4bedf1
Unknown <leafar_rafaelsm@hotmail.com>
2017-06-06 20:59:34 -0300

    Formularios de cadastro do adm, js ja pega os valores

--- javascript/adm.js
+++ javascript/adm.js
@@ -1,6 +1,44 @@
+function cadastrarAdministrador(){
+	name = document.getElementById('name').value;
+	email = document.getElementById('email').value;
+	password = document.getElementById('password').value;
+	cpassword = document.getElementById('cpassword').value;
+	if(password == cpassword){
+		adm = {name: name, email: email, password: password};
+		alert(JSON.stringify(adm));
+	} else {
+		alert('Senha errada!');
+	}
+}
+
 function cadastrarCliente(){
 	name = document.getElementById('name').value;
 	email = document.getElementById('email').value;
 	password = document.getElementById('password').value;
 	cpassword = document.getElementById('cpassword').value;
+	if(password == cpassword){
+		cliente = {name: name, email: email, password: password};
+		alert(JSON.stringify(cliente));
+	} else {
+		alert('Senha errada!');
+	}
+}
+
+function cadastrarProduto(){
+	name = document.getElementById('name').value;
+	desc = document.getElementById('desc').value;
+	price = document.getElementById('price').value;
+	qtd = document.getElementById('qtd').value;
+	foto = document.getElementById('foto').value;
+	produto = {name: name, desc: desc, price: price, qtd: qtd, foto: foto};
+	alert(JSON.stringify(produto));
+}
+
+function cadastrarServico(){
+	name = document.getElementById('name').value;
+	desc = document.getElementById('desc').value;
+	price = document.getElementById('price').value;
+	foto = document.getElementById('foto').value;
+	servico = {name: name, desc: desc, price: price, foto: foto};
+	alert(JSON.stringify(servico));
 }
