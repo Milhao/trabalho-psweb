a18222e0b767dbf0542a71211f68ed5405fa6345
Unknown <leafar_rafaelsm@hotmail.com>
2017-06-11 20:55:38 -0300

    função para gerar menu, precisa gerar cookie de login para finalizá-la

--- javascript/user.js
+++ javascript/user.js
@@ -36,3 +36,58 @@ function login(){
 		console.log(request.error.name + '\n\n' + request.error.message);
 	};
 }
+
+function menu(){
+	//if(logado){
+		let ul = document.getElementById('menu');
+		let li = document.createElement("li");
+		let li_ul = document.createElement("ul");
+		let li_ul_li1 = document.createElement("li");
+		let li_ul_li2 = document.createElement("li");
+
+		Animais = document.createElement("A");
+		Cadastrar = document.createElement("A");
+		Editar = document.createElement("A");
+		Animais.innerHTML = "Animais";
+		Cadastrar.innerHTML = "Cadastrar";
+		Cadastrar.href = "cadastrar-animal.html";
+		Editar.innerHTML = "Editar Dados";
+		Editar.href = "listar-animal.html";
+
+		li.appendChild(Animais);
+		li_ul_li1.appendChild(Cadastrar);
+		li_ul_li2.appendChild(Editar);
+		ul.appendChild(li);
+		li.appendChild(li_ul);
+		li_ul.appendChild(li_ul_li1);
+		li_ul.appendChild(li_ul_li2);
+
+		li = document.createElement("li");
+		li_ul = document.createElement("ul");
+		li_ul_li1 = document.createElement("li");
+		li_ul_li2 = document.createElement("li");
+		li_ul_li3 = document.createElement("li");
+
+		Dados = document.createElement("A");
+		Editar = document.createElement("A");
+		Endereco = document.createElement("A");
+		Senha = document.createElement("A");
+		Dados.innerHTML = "Dados Pessoais";
+		Editar.innerHTML = "Editar";
+		Editar.href = "editar-dados-pessoais.html";
+		Endereco.innerHTML = "Endereço";
+		Endereco.href = "editar-endereco.html";
+		Senha.innerHTML = "Alterar Senha";
+		Senha.href = "editar-senha.html";
+
+		li.appendChild(Dados);
+		li_ul_li1.appendChild(Editar);
+		li_ul_li2.appendChild(Endereco);
+		li_ul_li3.appendChild(Senha);
+		ul.appendChild(li);
+		li.appendChild(li_ul);
+		li_ul.appendChild(li_ul_li1);
+		li_ul.appendChild(li_ul_li2);
+		li_ul.appendChild(li_ul_li3);
+ 	//}
+}
