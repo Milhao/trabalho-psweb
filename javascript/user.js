da0048d761e51aff1f40acfcae966833928c9ff6
Unknown <leafar_rafaelsm@hotmail.com>
2017-06-12 22:31:36 -0300

    Só falta acertar a função isLogged, necessita do cookie

--- javascript/user.js
+++ javascript/user.js
@@ -4,141 +4,81 @@ function isLogged(){
 	return 0;
 }
 
-function menu(logado){
-	if(logado){
-		let header = document.getElementById('header');
-		let ul = document.getElementById('menu');
-		let li = document.createElement("li");
-		let li_ul = document.createElement("ul");
-		let li_ul_li1 = document.createElement("li");
-		let li_ul_li2 = document.createElement("li");
-		let carrinho = document.createElement("a");
-		let img = document.createElement("img");
+function login() {
+	password = document.getElementById('password').value;
+	email = document.getElementById('email').value;
 
-		Animais = document.createElement("A");
-		Cadastrar = document.createElement("A");
-		Editar = document.createElement("A");
-		Animais.innerHTML = "Animais";
-		Cadastrar.innerHTML = "Cadastrar";
-		Cadastrar.href = "cadastrar-animal.html";
-		Editar.innerHTML = "Editar Dados";
-		Editar.href = "listar-animal.html";
-
-		li.appendChild(Animais);
-		li_ul_li1.appendChild(Cadastrar);
-		li_ul_li2.appendChild(Editar);
-		ul.appendChild(li);
-		li.appendChild(li_ul);
-		li_ul.appendChild(li_ul_li1);
-		li_ul.appendChild(li_ul_li2);
-
-		li = document.createElement("li");
-		li_ul = document.createElement("ul");
-		li_ul_li1 = document.createElement("li");
-		li_ul_li2 = document.createElement("li");
-		li_ul_li3 = document.createElement("li");
-
-		Dados = document.createElement("A");
-		Editar = document.createElement("A");
-		Endereco = document.createElement("A");
-		Senha = document.createElement("A");
-		Dados.innerHTML = "Dados Pessoais";
-		Editar.innerHTML = "Editar";
-		Editar.href = "editar-dados-pessoais.html";
-		Endereco.innerHTML = "Endereço";
-		Endereco.href = "editar-endereco.html";
-		Senha.innerHTML = "Alterar Senha";
-		Senha.href = "editar-senha.html";
-
-		li.appendChild(Dados);
-		li_ul_li1.appendChild(Editar);
-		li_ul_li2.appendChild(Endereco);
-		li_ul_li3.appendChild(Senha);
-		ul.appendChild(li);
-		li.appendChild(li_ul);
-		li_ul.appendChild(li_ul_li1);
-		li_ul.appendChild(li_ul_li2);
-		li_ul.appendChild(li_ul_li3);
-
-		carrinho.href = "carrinho.html";
-		img.id = "shop-cart";
-		img.src = "../images/shopping-cart.png";
-		img.alt = "Carrinho de Compras";
-
-		carrinho.appendChild(img);
-
-		header.appendChild(carrinho);
- 	} else {
-		let header = document.getElementById('header');
-		let form = document.createElement("form");
-		let input1 = document.createElement("input");
-		let input2 = document.createElement("input");
-		let input3 = document.createElement("input");
-
-		form.className = "login-form";
-		form.method = "POST"
-
-		input1.id = "email";
-		input1.className = "login-input";
-		input1.type = "email";
-		input1.name = "email";
-		input1.placeholder = "E-mail";
-
-		input2.id = "password";
-		input2.className = "login-input";
-		input2.type = "password";
-		input2.name = "password";
-		input2.placeholder = "Senha";
-
-		input3.className = "login-input login-button";
-		input3.type = "button";
-		input3.value = "Entrar";
-		input3.onclick = function() {
-			password = document.getElementById('password').value;
-			email = document.getElementById('email').value;
-
-			let active = dataBase.result;
-			let transaction = active.transaction(["cliente"], "readonly");
-			let objectStore = transaction.objectStore("cliente");
-			let index = objectStore.index("email");
-			let request = index.get(email);
+	let active = dataBase.result;
+	let transaction = active.transaction(["cliente"], "readonly");
+	let objectStore = transaction.objectStore("cliente");
+	let index = objectStore.index("email");
+	let request = index.get(email);
 			
-			request.onsuccess = function () {
-				if (request.result !== undefined) {
-					if(request.result.password !== password)
+	request.onsuccess = function () {
+		if (request.result !== undefined) {
+			if(request.result.password !== password)
+				alert("Senha errada!");
+			else{
+				window.location.assign("index.html");
+			}
+		} else {
+			let transaction2 = active.transaction(["adm"], "readonly");
+			let objectStore2 = transaction2.objectStore("adm");
+			let index2 = objectStore2.index("email");
+			let request2 = index2.get(email);
+
+			request2.onsuccess = function () {
+				if (request2.result !== undefined) {
+					if(request2.result.password !== password)
 						alert("Senha errada!");
-					else{
-						//gerar cookie
-						window.location.assign("index.html");
-					}
-				} else {
-					let transaction2 = active.transaction(["adm"], "readonly");
-					let objectStore2 = transaction2.objectStore("adm");
-					let index2 = objectStore2.index("email");
-					let request2 = index2.get(email);
-
-					request2.onsuccess = function () {
-						if (request2.result !== undefined) {
-							if(request2.result.password !== password)
-								alert("Senha errada!");
-							else
-								window.location.assign("index-admin.html");
-						} else
-							alert("E-mail não encontrado!");
-					};
-				}
+					else
+						window.location.assign("index-admin.html");
+				} else
+					alert("E-mail não encontrado!");
 			};
+		}
+	};
 
-			request.onerror = function (e) {
-				console.log(request.error.name + '\n\n' + request.error.message);
-			};
-		};
+	request.onerror = function (e) {
+		console.log(request.error.name + '\n\n' + request.error.message);
+	};
+}
 
-		form.appendChild(input1);
-		form.appendChild(input2);
-		form.appendChild(input3);
-
-		header.appendChild(form);
+function menu(logado){
+	let outerHTML = '';
+	if(logado){
+		outerHTML = '<a href="index.html"><img id="petshop-logo" src="../images/petshop-logo-half-white.png" alt="Logo Petshop"/></a>\n\
+					<ul>\n\
+						<li><a href="listar-produtos.html">Produtos</a></li>\n\
+						<li><a href="servico.html">Serviços</a></li>\n\
+						<li><a href="">Animais</a>\n\
+							<ul>\n\
+								<li><a href="cadastrar-animal.html">Cadastrar</a></li>\n\
+								<li><a href="listar-animal.html">Editar Dados</a></li>\n\
+							</ul>\n\
+						</li>\n\
+						<li><a href="">Dados Pessoais</a>\n\
+							<ul>\n\
+								<li><a href="editar-dados-pessoais.html">Editar</a></li>\n\
+								<li><a href="editar-endereco.html">Endereço</a></li>\n\
+								<li><a href="editar-senha.html">Alterar Senha</a></li>\n\
+							</ul>\n\
+						</li>\n\
+					</ul>\n\
+					<a href="carrinho.html"><img id="shop-cart" src="../images/shopping-cart.png" alt="Carrinho de Compras"/></a>';
+		document.getElementById('menu').innerHTML = outerHTML;
+	} else {
+		outerHTML = '<a href="index.html"><img id="petshop-logo" src="../images/petshop-logo-half-white.png" alt="Logo Petshop"/></a>\n\
+					<ul>\n\
+						<li><a href="listar-produtos.html">Produtos</a></li>\n\
+						<li><a href="servico.html">Serviços</a></li>\n\
+					</ul>\n\
+					<form class="login-form" action="" method="POST">\n\
+						<input id="email" class="login-input" type="email" name="email" placeholder="E-mail"/>\n\
+						<input id="password" class="login-input" type="password" name="password" placeholder="Senha"/>\n\
+						<input class="login-input login-button"class="login-button" class="" type="submit" value="Entrar" onclick="login()"/>\n\
+					</form>';
+		document.getElementById('menu').innerHTML = outerHTML;
  	}
 }
 
